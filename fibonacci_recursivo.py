95f8d71b4e56d0f3e41ba9e1c4c53d185996a6f8
Lucas Navarro <lucassessim@hotmail.com>
2026-08-16 20:02:39 -0300

    Add files via upload

---
+++ fibonacci_recursivo.py
@@ -0,0 +1,18 @@
+
+#solução recursiva de fibonacci
+#ex sequencia fibonacci: 0 - 1 - 1 - 2 - 3 - 5 - 8 - 13 - 21...
+
+def fib(n):
+    if n < 0:
+        return "Entrada Inválida"
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    if n == 2:
+        return 1
+    return fib(n-1) + fib(n-2)
+
+n = int(input())
+print(fib(n))
+
